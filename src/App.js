e0bedb518ba13b779fd44070c8f49106dafcc59d
tavolafourcade <tavolafourcade@gmail.com>
2021-07-03 20:25:30 -0500

    Created the App.js of the example of Hook UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import RickMortyClass from './components/RickMortyClass';
+import RickMortyFn from './components/RickMortyFn';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="Home">Home</Link>
+      <Link to="RickMortyClass">RickMortyClass</Link>
+      <Link to="RickMortyFn">RickMortyFn</Link>
+      <hr/>
+
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/rickmortyclass' component={RickMortyClass}></Route>
+      <Route exact path='/rickmortyfn' component={RickMortyFn}></Route>
     </div>
   );
 }
